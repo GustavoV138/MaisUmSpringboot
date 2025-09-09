2c89df84103a6b4972d352165b7e3e77f044d1d8
Gustavo Vieira <joao.g138+github@gmail.com>
2025-09-09 17:49:09 -0300

    Criacao do projeto e Configuracao app.properties

---
+++ src/main/java/com/general/mais_um_springboot/MaisUmSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.general.mais_um_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisUmSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaisUmSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/general/mais_um_springboot/MaisUmSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.general.mais_um_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaisUmSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
